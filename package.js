f4029d161fcf35cc7541548fd84f474d998e7102
Joshua Ohlman <codewriterohs@gmail.com>
2015-03-11 15:27:20 -0500

    update package.js

--- package.js
+++ package.js
@@ -2,9 +2,9 @@ Package.describe({
   name: 'cwohlman:materialize-modal',
   version: '0.0.1',
   // Brief, one-line summary of the package.
-  summary: '',
+  summary: 'Create modals in an app using materialize css',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/cwohlman/meteor-materialize-modal',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
